a7dfb257db2e79d43985f74abc8344c2ff2e8ebe
hoangnh803 <hoang.nh803@gmail.com>
2025-01-06 11:20:21 +0700

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://voucherapp.me/api', 
+  baseURL: 'http://voucherapp.me/api', 
   headers: {
     'Content-Type': 'application/json',
   },
